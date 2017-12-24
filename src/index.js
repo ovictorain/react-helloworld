f69069bfa2183fd561ed7a8416f137b57cdf2943
weiyanqun <wei_yanqun@hotmail.com>
2017-12-24 20:08:24 +0800

    Forms (3) : The select Tag

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import EssayForm from './EssayForm';
+import FlavorForm from './FlavorForm';
 import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(
-   <EssayForm />, document.getElementById('root'));
+   <FlavorForm />, document.getElementById('root'));
 registerServiceWorker();
