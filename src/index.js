aaed47bde90bc953fadf4b3f27ad29cba7af8417
weiyanqun <wei_yanqun@hotmail.com>
2017-12-24 14:42:01 +0800

    Class Components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Welcome from './FuncWelcome';
+import Welcome from './ClassWelcome';
 import registerServiceWorker from './registerServiceWorker';
 
 
