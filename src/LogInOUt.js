78e74abfca0e75a0a9e382c2745371198507511e
weiyanqun <wei_yanqun@hotmail.com>
2017-12-24 19:19:44 +0800

    Conditional Rendering (1) : Element Variables

---
+++ src/LogInOUt.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+export { LoginButton, LogoutButton};
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Greeting from './Greeting';
+import LoginControl from './LoginControl';
 import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(
   // Try changing to isLoggedIn={true}:
-  <Greeting isLoggedIn={false} />, document.getElementById('root'));
+  <LoginControl />, document.getElementById('root'));
 registerServiceWorker();
