66afc702d4f639cf6db071c537a58b2b37528abc
weiyanqun <wei_yanqun@hotmail.com>
2017-12-24 20:02:14 +0800

    Forms (1) : Controlled Components

--- src/index.js
+++ src/index.js
@@ -2,20 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import LoginControl from './LoginControl';
+import NameForm from './NameForm';
 import registerServiceWorker from './registerServiceWorker';
 
-function NumberList(props) {
-  const numbers = props.numbers;
-  const listItems = numbers.map((number) =>
-    <li key={number.toString()}>{number}</li>
-  );
-  return (
-    <ul>{listItems}</ul>
-  );
-}
 
-const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
-   <NumberList numbers={numbers} />, document.getElementById('root'));
+   <NameForm />, document.getElementById('root'));
 registerServiceWorker();
