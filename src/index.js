faed234c9e4d48547379ecafb8691e16c2b28ccc
weiyanqun <wei_yanqun@hotmail.com>
2017-12-24 15:16:07 +0800

    Extracting Components (1)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Comment from './Comment';
 import registerServiceWorker from './registerServiceWorker';
 
+const comment = {
+  date: new Date(),
+  text:
+    'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl:
+      'http://placekitten.com/g/64/64',
+  },
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Comment date={comment.date} text={comment.text} author={comment.author} />, document.getElementById('root'));
 registerServiceWorker();
