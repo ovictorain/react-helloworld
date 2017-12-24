93ce6d296429b32ff9f25853b05afb39f3c87382
weiyanqun <wei_yanqun@hotmail.com>
2017-12-24 20:05:26 +0800

    Forms (2) : The textarea Tag

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import NameForm from './NameForm';
+import EssayForm from './EssayForm';
 import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(
-   <NameForm />, document.getElementById('root'));
+   <EssayForm />, document.getElementById('root'));
 registerServiceWorker();
