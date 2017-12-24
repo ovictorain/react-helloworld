0f6acf0036d57a539c18ca322bcb3aecc2ecc769
weiyanqun <wei_yanqun@hotmail.com>
2017-12-24 20:16:26 +0800

    Forms (4) : Handling Multiple Inputs

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import FlavorForm from './FlavorForm';
+import Reservation from './Reservation';
 import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(
-   <FlavorForm />, document.getElementById('root'));
+   <Reservation />, document.getElementById('root'));
 registerServiceWorker();
