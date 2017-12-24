04169b400162e9ac8b8c52275cd89ef86575e788
weiyanqun <wei_yanqun@hotmail.com>
2017-12-24 16:24:34 +0800

    State and Lifecycle (2): Converting a Function to a Class

--- src/Clock.js
+++ src/Clock.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Clock(props) {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+class Clock extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
-
 export default Clock
