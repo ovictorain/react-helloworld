6ae24607500a1ee4ea1d72d212c461c7dc56c9c8
weiyanqun <wei_yanqun@hotmail.com>
2017-12-24 15:47:56 +0800

    Extracting Components (2)

--- src/Comment.js
+++ src/Comment.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import UserInfo from './UserInfo';
 
 function formatDate(date) {
   return date.toLocaleDateString();
@@ -8,15 +9,7 @@ function formatDate(date) {
 function Comment(props) {
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <img className="Avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
+      <UserInfo user={props.author} />
       <div className="Comment-text">
         {props.text}
       </div>
