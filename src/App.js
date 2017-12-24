583cb3b2dfb9e32338781f03c6b345e7a8af059d
weiyanqun <wei_yanqun@hotmail.com>
2017-12-24 14:27:43 +0800

    Updating the Rendered Element (App.js中的element要放到class Appd的render()方法里，否则作为变量不刷秒) (每次都刷新RenderDOM，也只刷新了修改部分，但仍不建议)

--- src/App.js
+++ src/App.js
@@ -2,50 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-  firstName: 'Little',
-  lastName: 'Victor'
-};
-
-const element = (
-  <span>
-    Hello, {formatName(user)}!
-  </span>
-);
-
 class App extends Component {
-  /**
-   * use constructor
-   */
-  // constructor(firstName, lastName) {
-  //   super();
-  //
-  //   this.firstName = firstName;
-  //   this.lastName = lastName;
-  // }
-
-  /**
-   * use method
-   */
-  get name() {
-    return <span>Hello, {formatName(user)}!</span>
-  }
-  
   render() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <div className="App-intro">
             {element}
-            {/* {this.name} */}
-        </p>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function tick() {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  registerServiceWorker();
+}
+
+setInterval(tick, 1000);
