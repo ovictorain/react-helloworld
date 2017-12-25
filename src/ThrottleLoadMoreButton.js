458c538f8e555a3541f8b2f67e041595f3f456c6
weiyanqun <wei_yanqun@hotmail.com>
2017-12-25 13:16:25 +0800

    Passing Functions to Components : Throttle

---
+++ src/ThrottleLoadMoreButton.js
@@ -0,0 +1,25 @@
+import throttle from 'lodash.throttle';
+import React from 'react';
+
+
+class ThrottleLoadMoreButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClickThrottled = throttle(this.handleClick, 1000);
+  }
+
+  componentWillUnmount() {
+    this.handleClickThrottled.cancel();
+  }
+
+  render() {
+    return <button onClick={this.handleClickThrottled}>Load More</button>;
+  }
+
+  handleClick() {
+    this.props.loadMore();
+  }
+}
+
+export default ThrottleLoadMoreButton;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import FilterableProductTable from './FilterableProductTable';
+import ThrottleLoadMoreButton from './ThrottleLoadMoreButton';
 import registerServiceWorker from './registerServiceWorker';
 
 
-
-const PRODUCTS = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
-
-ReactDOM.render(<FilterableProductTable products={PRODUCTS} />, document.getElementById('root'));
+ReactDOM.render(<ThrottleLoadMoreButton loadMore={() => {console.log(`Throttling prevents a function from being called more than once in a given window of time.
+The example below throttles a “click” handler to prevent calling it more than once per second.`);}}/>, document.getElementById('root'));
 registerServiceWorker();
