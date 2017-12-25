1c4bfa5cd5a4ce89b1b026bb8960227af263ff7b
weiyanqun <wei_yanqun@hotmail.com>
2017-12-25 10:19:57 +0800

    Thinking in React : FilterableProductTable

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Calculator from './Calculator';
+import FilterableProductTable from './FilterableProductTable';
 import registerServiceWorker from './registerServiceWorker';
 
 
-ReactDOM.render(
-   <Calculator />, document.getElementById('root'));
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+ReactDOM.render(<FilterableProductTable products={PRODUCTS} />, document.getElementById('root'));
 registerServiceWorker();
